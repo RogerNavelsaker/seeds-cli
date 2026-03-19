dbb923289350a11d6ca6c48aa6c4babb0f18b093
Roger Navelsaker <roger.navelsaker@gmail.com>
2026-03-19 13:36:11 +0100

    Remove extra flake attrs

--- flake.nix
+++ flake.nix
@@ -33,14 +33,9 @@
         };
       });
     in {
-      packages = forAllSystems ({ pkgs }:
-        let
-          package = pkgs.callPackage ./nix/package.nix { };
-        in {
-          default = package;
-          seeds = package;
-          sd = package.sd;
-        });
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.callPackage ./nix/package.nix { };
+      });
 
       devShells = forAllSystems ({ pkgs }: {
         default = pkgs.mkShell {
